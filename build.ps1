cd583f9ab0c2b587f126fab6b79c03806ee62305
Daniel R. Herber <daniel.herber@colostate.edu>
2024-03-30 10:16:01 -0600

    Added slides - Animated and some minor updates to build script

--- build.ps1
+++ build.ps1
@@ -22,7 +22,13 @@ pdflatex -draftmode -interaction=batchmode  -synctex=0 --aux-directory=.build $f
 pdflatex -interaction=batchmode  -synctex=0 --aux-directory=.build $filename
 pdflatex -interaction=batchmode -synctex=0 --aux-directory=.build $filename
 
+$filenamepdf = $filename+".pdf"
+
+./../../../.config-specific/pdfsizeopt/pdfsizeopt --do-optimize-images=no $filenamepdf $filenamepdf
+
+Write-Host $(" ")
 Write-Host $("--- Finished "+$filename+" ---")
+Write-Host $(" ")
 
 #----------------------------------------------------------------------------------
 # Dark Version
@@ -46,7 +52,43 @@ pdflatex -draftmode -interaction=batchmode -synctex=0 --aux-directory=.build $da
 pdflatex -interaction=batchmode -synctex=0 --aux-directory=.build $darkfilename
 pdflatex -interaction=batchmode -synctex=0 --aux-directory=.build $darkfilename
 
+$darkfilenamepdf = $darkfilename+".pdf"
+
+./../../../.config-specific/pdfsizeopt/pdfsizeopt --do-optimize-images=no $darkfilenamepdf $darkfilenamepdf
+
+Write-Host $(" ")
 Write-Host $("--- Finished "+$darkfilename+" ---")
+Write-Host $(" ")
+
+#----------------------------------------------------------------------------------
+# Animated Version
+$animatedfilename = $filename+" - Animated"
+
+$FolderName = ".build"
+if (Test-Path $FolderName) {
+    Remove-Item $FolderName -Recurse -Force
+}
+else
+{
+
+}
+
+pdflatex -draftmode -interaction=batchmode -synctex=0 --aux-directory=.build $animatedfilename
+
+biber --output-directory=.build $animatedfilename
+
+pdflatex -draftmode -interaction=batchmode -synctex=0 --aux-directory=.build $animatedfilename
+pdflatex -draftmode -interaction=batchmode -synctex=0 --aux-directory=.build $animatedfilename
+pdflatex -interaction=batchmode -synctex=0 --aux-directory=.build $animatedfilename
+pdflatex -interaction=batchmode -synctex=0 --aux-directory=.build $animatedfilename
+
+$animatedfilenamepdf = $animatedfilename+".pdf"
+
+./../../../.config-specific/pdfsizeopt/pdfsizeopt --do-optimize-images=no $animatedfilenamepdf $animatedfilenamepdf
+
+Write-Host $(" ")
+Write-Host $("--- Finished "+$animatedfilename+" ---")
+Write-Host $(" ")
 
 #----------------------------------------------------------------------------------
 # end timer
